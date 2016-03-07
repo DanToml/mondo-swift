cb6c906a30dd910d4de7234c1ac0806f71c937a8
Daniel Tomlinson <Dan@Tomlinson.io>
2016-03-07 19:07:45 +0000

    Initial Commit

---
+++ MondoAPI/MondoAPI.h
@@ -0,0 +1,19 @@
+//
+//  MondoAPI.h
+//  MondoAPI
+//
+//  Created by Daniel Tomlinson on 07/03/2016.
+//  Copyright © 2016 Rocket Apps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MondoAPI.
+FOUNDATION_EXPORT double MondoAPIVersionNumber;
+
+//! Project version string for MondoAPI.
+FOUNDATION_EXPORT const unsigned char MondoAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MondoAPI/PublicHeader.h>
+
+
